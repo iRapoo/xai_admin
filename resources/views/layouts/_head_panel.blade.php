e196f3ebf8102693d3e7e99c1c720cdc3286db54
Rapoo <rapoo.git@gmail.com>
2018-12-25 17:14:08 +0300

    Clean template

---
+++ resources/views/layouts/_head_panel.blade.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
+
+<title>Панель управления</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+<!-- animation CSS -->
+<link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<!-- color CSS -->
+<link href="{{ asset('css/colors/default.css') }}" id="theme" rel="stylesheet">
---
+++ resources/views/layouts/_scripts_panel.blade.php
@@ -0,0 +1,11 @@
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="{{ asset('jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ asset('bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!--slimscroll JavaScript -->
+<script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
+<!--Wave Effects -->
+<script src="{{ asset('js/waves.js') }}"></script>
+<!-- Custom Theme JavaScript -->
+<script src="{{ asset('js/custom.js') }}"></script>
